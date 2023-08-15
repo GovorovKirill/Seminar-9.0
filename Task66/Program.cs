f9c0f58d625f43e2b7b0435ba5f8120ade4ac68d
Kirill <kirillgovorov@gmail.com>
2023-08-15 21:04:48 +0300

    Task66 is done

--- Task66/Program.cs
+++ Task66/Program.cs
@@ -3,6 +3,8 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
+using System.Diagnostics.CodeAnalysis;
+
 Console.WriteLine("Введите первое натуральное число");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе натуральное число");
@@ -10,41 +12,37 @@ int number2 = Convert.ToInt32(Console.ReadLine());
 
 
 int sumNaturalNumbersRange = SumNaturalNumbersRange(number1, number2);
-Console.WriteLine($"{sumNaturalNumbersRange}");
+Console.WriteLine(sumNaturalNumbersRange);
 
 
-int SumNaturalNumbersRange(int n1, int n2, int sum = 0)
+int SumNaturalNumbersRange(int n1, int n2)
 {
-    if (n1 < 1 || n2 < 1)
+    if (n1 < 0 || n2 < 0)
     {
         Console.WriteLine("Одно или оба числа не натуральные");
+        return 0;
     }
 
     else
     {
+
         if (n1 > n2)
         {
-            while (n2 <= n1)
-            {
-                sum = sum + n2;
-                n2++;
-            }
+            if (n1 < n2) return 0;
+            return n1 + SumNaturalNumbersRange(n1 - 1, n2);
         }
 
-        else if (n1 < n2)
-        {
-            while (n1 <= n2)
-            {
-                sum = sum + n1;
-                n1++;
-            }
-        }
+
 
         else
         {
-            sum = sum + n1;
+            if (n1 < n2)
+            {
+                if (n2 < n1) return 0;
+                return n2 + SumNaturalNumbersRange(n2 - 1, n1);
+
+            }
         }
     }
-
-    return sum;
+    return n1;
 }
